c05068b962f7c79d715904cf8f2d5eca5b079f76
Tony Morris <tmorris@tmorris.net>
2018-04-16 18:37:28 +1000

    scraped using IP address 192.185.46.73

---
+++ wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php
@@ -0,0 +1,203 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss/' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [https://www.imagely.com/wordpress-gallery-plugin/nextgen-gallery/]]]></generator>
+		<title>Archer Falls Airfield</title>
+		<description>Aviators Paradise</description>
+		<link><![CDATA[http://archerfalls.com.au]]></link>
+		<atom:link rel='self' href='http://archerfalls.com.au/wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php' type='application/rss+xml' />
+		<atom:link rel='next' href='http://archerfalls.com.au/wp-content/plugins/nextgen-gallery/products/photocrati_nextgen/modules/ngglegacy/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0662.jpg]]></link>
+			<guid>image-id:29</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0662.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0662.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0649.jpg]]></link>
+			<guid>image-id:28</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0649.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0649.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0633.jpg]]></link>
+			<guid>image-id:27</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0633.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0633.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0627.jpg]]></link>
+			<guid>image-id:26</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0627.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0627.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0626.jpg]]></link>
+			<guid>image-id:25</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0626.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0626.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0624.jpg]]></link>
+			<guid>image-id:24</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0624.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0624.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0619.jpg]]></link>
+			<guid>image-id:23</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0619.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0619.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0615.jpg]]></link>
+			<guid>image-id:22</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0615.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0615.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0613.jpg]]></link>
+			<guid>image-id:21</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0613.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0613.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0558.jpg]]></link>
+			<guid>image-id:20</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0558.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0558.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Spitfire Mk26]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0611.jpg]]></link>
+			<guid>image-id:19</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/spitfire/dscf0611.jpg' medium='image' />
+			<media:title><![CDATA[Spitfire Mk26]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/spitfire/thumbs/thumbs_dscf0611.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[spitfire]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Archer Falls Airfield "The Old Winery"]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-227.jpg]]></link>
+			<guid>image-id:18</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-227.jpg' medium='image' />
+			<media:title><![CDATA[Archer Falls Airfield "The Old Winery"]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/thumbs/thumbs_picture-227.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-225.jpg]]></link>
+			<guid>image-id:17</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-225.jpg' medium='image' />
+			<media:title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/thumbs/thumbs_picture-225.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-139.jpg]]></link>
+			<guid>image-id:16</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-139.jpg' medium='image' />
+			<media:title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/thumbs/thumbs_picture-139.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[The Flight Deck Bar]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-130.jpg]]></link>
+			<guid>image-id:15</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-130.jpg' medium='image' />
+			<media:title><![CDATA[The Flight Deck Bar]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/thumbs/thumbs_picture-130.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-124.jpg]]></link>
+			<guid>image-id:14</guid>
+			<media:content url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/picture-124.jpg' medium='image' />
+			<media:title><![CDATA[Archer Falls Airfield's "The Old Winery"]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://archerfalls.com.au/wp-content/gallery/the-clubhouse/thumbs/thumbs_picture-124.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Archer Falls Airfield (http://archerfalls.com.au)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
